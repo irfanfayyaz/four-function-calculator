797c13b6cb9eb311d21d03e3bbe01fa88084dbd0
irfanfayyaz <irfan.47326@yahoo.com>
2024-04-22 16:05:28 +0500

    tweak the text and added console at the end

--- cal.js
+++ cal.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
     { message: "Select operator to perform operation",
         type: "list",
         name: "operator",
@@ -25,3 +25,4 @@ else if (answer.operator === "Division") {
 else {
     console.log("Select valid operator");
 }
+console.log('THE END');
